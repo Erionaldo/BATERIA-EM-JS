41572567985038729123fe7550043d160177ed83
Erionaldo <122840626+Erionaldo@users.noreply.github.com>
2023-02-22 21:03:29 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
-document.body.addEventListener('keyup', (event)=> {
+document.body.addEventListener('keydown', (event) => {
     playSound(event.code.toLowerCase());
+  });
+  
+  document.querySelector('.composer button').addEventListener('click', () => {
+    let song = document.querySelector('input').value;
+  
+    if (song !== '') {
+      let songArray = song.split('');
+      playComposition(songArray);
+    }
   });
   
   function playSound(sound) {
     let audioElement = document.querySelector(`#s_${sound}`);
-  
+    
     if (audioElement) {
+      audioElement.currentTime = 0;
       audioElement.play();
     }
   }
   
+  function playComposition(songArray) {
+    let wait = 0
+    for(let songItem of songArray) {
+        setTimeout(() => { playSound(`key${songItem}`); }, wait);
+        
+        wait += 250;
+       
+    }
+  }
